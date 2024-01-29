bed2813937675cba0a43d81db679bfeba505f54e
Raphael Raberger <Raphael.Raberger@htl-wels.at>
2024-01-29 17:33:09 +0100

    Added sensor

---
+++ Physim/Sensors/Sensor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physim.Sensors
+{
+    public class Sensor
+    {
+        public string Name { get; set; } = "";
+
+        public virtual float getValue(PhysicsObject obj, Simulation.SimulationUpdateStepInfo info)
+        {
+            return 0f;
+        }
+
+    }
+}
